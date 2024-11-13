fa17b0cd4a1fdf1d9ec2c5c04f19236ec0a43dfd
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2024-11-12 17:26:09 -0800

    add a traversal test

---
+++ test/traverse.ml
@@ -0,0 +1,18 @@
+let () =
+  let items = [ "a"; "b"; "c"; "d"; "e"; "f"; "g" ] in
+  let options =
+    [
+      [ "c"; "e" ];
+      [ "a"; "d"; "g" ];
+      [ "b"; "c"; "f" ];
+      [ "a"; "d"; "f" ];
+      [ "b"; "g" ];
+      [ "d"; "e"; "g" ];
+    ]
+  in
+  let cur = Dlx.Algox.mk ~items ~_options:options in
+
+  let node_of = Dlx.Algox.find ~name:"c" cur in
+  CCFormat.printf "@.Name: %a, id: %d@."
+    CCFormat.(some string)
+    node_of.name node_of.id
