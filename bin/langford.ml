27b05280b9a4dc2131b0b15d89bafb429745d896
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2025-02-25 17:42:07 -0800

    generalized langford pairs

--- bin/langford.ml
+++ bin/langford.ml
@@ -1,23 +1,42 @@
-let () =
-  let items = [ "1"; "2"; "3"; "s1"; "s2"; "s3"; "s4"; "s5"; "s6" ] in
-  let options =
-    [
-      [ "1"; "s1"; "s3" ];
-      [ "1"; "s2"; "s4" ];
-      [ "1"; "s3"; "s5" ];
-      [ "1"; "s4"; "s6" ];
-      [ "2"; "s1"; "s4" ];
-      [ "2"; "s2"; "s5" ];
-      [ "2"; "s3"; "s6" ];
-      [ "3"; "s1"; "s5" ];
-      [ "3"; "s2"; "s6" ];
-    ]
+let generate_items n =
+  let nums = List.init n (fun i -> string_of_int (i + 1)) in
+  let slots = List.init (2 * n) (fun i -> "s" ^ string_of_int (i + 1)) in
+  nums @ slots
+
+let generate_options n =
+  let rec generate k acc =
+    if k > n then
+      acc
+    else (
+      let rec place i acc =
+        if i + k + 1 >= 2 * n then
+          acc
+        else (
+          let option =
+            [
+              string_of_int k;
+              "s" ^ string_of_int (i + 1);
+              "s" ^ string_of_int (i + k + 2);
+            ]
+          in
+          place (i + 1) (option :: acc)
+        )
+      in
+      generate (k + 1) (place 0 acc)
+    )
   in
+  generate 1 []
+
+(* Compare https://oeis.org/A014552 *)
+let () =
+  let n = 8 in
+  let items = generate_items n in
+  let options = generate_options n in
   let cur = Dlx.Algox.mk ~items ~options in
-  CCFormat.printf "Items = %a@.Options = %a@.Solution(s) = %a"
+  CCFormat.printf "Items = %a@.Options = %a@.Solution(s) = %d"
     CCFormat.Dump.(list string)
     cur.items
     CCFormat.Dump.(list (list string))
     cur.options
-    CCFormat.Dump.(list (list (list string)))
-    (Dlx.Algox.solve_dlx cur)
+    (* CCFormat.Dump.(list (list (list string))) *)
+    (CCList.length (Dlx.Algox.solve_dlx cur))
