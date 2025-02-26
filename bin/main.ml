8e5f5b5e2bac5eed30cd020424720bb10f58a96a
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2025-02-25 21:25:37 -0800

    Update README

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,19 @@
 let () = Printexc.record_backtrace true
 
 let () =
+  let items = [ "a"; "b"; "c"; "d" ] in
+  let options = [ [ "a"; "d" ]; [ "a"; "b" ]; [ "c"; "b" ] ] in
+  let cur = Dlx.Algox.mk ~items ~options in
+  CCFormat.printf "%a@." Dlx.Algox.pp cur;
+  CCFormat.printf "Items = %a@.Options = %a@.Solution = %a"
+    CCFormat.Dump.(list string)
+    cur.items
+    CCFormat.Dump.(list (list string))
+    cur.options
+    CCFormat.Dump.(list (list (list string)))
+    (Dlx.Algox.solve_dlx cur)
+
+let () =
   let items = [ "a"; "b"; "c"; "d"; "e"; "f"; "g" ] in
   let options =
     [ [ "c"; "e" ]; [ "b"; "f" ]; [ "a"; "d"; "g" ]; [ "d" ]; [ "a"; "g" ] ]
